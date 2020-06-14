d2d740062422d5e381129d2e2492eb5486240b0d
Iason Tsardanidis <jasontsardanidis@gmail.com>
2020-06-14 11:59:15 +0300

    Add files via upload

---
+++ Iason/clean_text.py
@@ -0,0 +1,167 @@
+# Clean Text Class
+
+
+import csv  # for slang
+import os
+import re  # regex
+import string  # punct
+import numpy as np
+import pandas as pd
+import emoji  # for emoji
+import nltk
+import sklearn
+from gensim.models import Word2Vec
+
+from nltk.corpus import stopwords  # stopwords
+from nltk.stem import PorterStemmer,SnowballStemmer  # stemming
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+
+class CleanText(BaseEstimator, TransformerMixin):
+    
+    def remove_mentions(self, input_text):
+        '''
+        Remove mentions, like @Mplamplampla
+        '''
+        return re.sub(r'@+', '', input_text)
+    
+    def remove_urls(self, input_text):
+        '''
+        Remove the urls mention in a tweet
+        '''
+        input_text  = ' '.join([w for w in input_text.split(' ') if '.com' not in w])
+        return re.sub(r'http.?://[^\s]+[\s]?', '', input_text)
+    
+    def emoji_oneword(self, input_text):
+        # By compressing the underscore, the emoji is kept as one word
+        input_text = emoji.demojize(input_text)
+        input_text = input_text.replace('_','')
+        input_text = input_text.replace(':','')
+        return input_text
+    
+    def possessive_pronouns(self, input_text):
+        '''
+        Remove the possesive pronouns, because otherwise after tokenization we will end up with a word and an s
+        Example: government's --> ["government", "s"]
+        '''
+        return input_text.replace("'s", "")
+    
+    def characters(self, input_text):
+        '''
+        Remove special and redundant characters that may appear on a tweet and that don't really help in our analysis
+        '''
+        input_text = input_text.replace("\r", " ") # Carriage Return
+        input_text = input_text.replace("\n", " ") # Newline
+        input_text = " ".join(input_text.split()) # Double space
+        input_text = input_text.replace('"', '') # Quotes
+        return input_text
+    
+    def remove_punctuation(self, input_text):
+        '''
+        Remove punctuation and specifically these symbols '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        '''
+        punct = string.punctuation # string with all the punctuation symbols '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+        trantab = str.maketrans(punct, len(punct)*' ')  # Every punctuation symbol will be replaced by a space
+        return input_text.translate(trantab)
+    
+    def remove_digits(self, input_text):
+        '''
+        Remove numbers
+        '''
+        return re.sub('\d+', '', input_text)
+    
+    def to_lower(self, input_text):
+        '''
+        Convert all the sentences(words) to lowercase
+        '''
+        return input_text.lower()
+    
+    def remove_stopwords(self, input_text):
+        '''
+        Remove stopwords (refers to the most common words in a language)
+        '''
+        stopwords_list = stopwords.words('english')
+        # Some words which might indicate a certain sentiment are kept via a whitelist
+        whitelist = ["n't", "not", "no"]
+        words = input_text.split() 
+        clean_words = [word for word in words if (word not in stopwords_list or word in whitelist) and len(word) > 1] 
+        return " ".join(clean_words) 
+    
+    def stemming(self, input_text):
+        '''
+        Reduce the words to their stem
+        '''
+        porter = PorterStemmer()
+        words = input_text.split() 
+        stemmed_words = [porter.stem(word) for word in words]
+        return " ".join(stemmed_words)
+    
+    def encode_decode(self, input_text):
+        '''
+        Remove weird characters that are result of encoding problems
+        '''
+        return  " ".join([k.encode("ascii", "ignore").decode() for k in input_text.split(" ")])
+    
+    
+    def translator(self, input_text):
+        '''
+        Transform abbrevations to normal words
+        Example: asap --> as soon as possible
+        '''
+        input_text = input_text.split(" ")
+        j = 0
+        for _str in input_text:
+            # File path which consists of Abbreviations.
+            fileName = r"slang.txt"
+            # File Access mode [Read Mode]
+            accessMode = "r"
+            with open(fileName, accessMode) as myCSVfile:
+                # Reading file as CSV with delimiter as "=", so that 
+                # abbreviation are stored in row[0] and phrases in row[1]
+                dataFromFile = csv.reader(myCSVfile, delimiter="=")
+                # Removing Special Characters.
+                _str = re.sub('[^a-zA-Z0-9-_.]', '', _str)
+                for row in dataFromFile:
+                    # Check if selected word matches short forms[LHS] in text file.
+                    if _str.upper() == row[0]:
+                        # If match found replace it with its appropriate phrase in text file.
+                        input_text[j] = row[1]
+                myCSVfile.close()
+            j = j + 1
+        
+        return(' '.join(input_text))
+    
+    def fit(self, X, y=None, **fit_params):
+        return self
+    
+    def transform(self, X, **transform_params):
+        clean_X = (X.apply(self.translator)
+                    .apply(self.remove_mentions)
+                    .apply(self.remove_urls)
+                    .apply(self.emoji_oneword)
+                    .apply(self.possessive_pronouns)
+                    .apply(self.remove_punctuation)
+                    .apply(self.remove_digits)
+                    .apply(self.encode_decode)
+                    .apply(self.characters)
+                    .apply(self.to_lower)
+                    .apply(self.remove_stopwords)
+                    .apply(self.stemming))
+        return clean_X
+    
+    def transform_no_stem(self, X, **transform_params):
+        clean_X = (X.apply(self.translator)
+                    .apply(self.remove_mentions)
+                    .apply(self.remove_urls)
+                    .apply(self.emoji_oneword)
+                    .apply(self.possessive_pronouns)
+                    .apply(self.remove_punctuation)
+                    .apply(self.remove_digits)
+                    .apply(self.encode_decode)
+                    .apply(self.characters)
+                    .apply(self.to_lower)
+                    .apply(self.remove_stopwords))
+        return clean_X
